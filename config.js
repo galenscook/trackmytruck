3ec2d7af085d8a8381bd74fd2ccba57dcde3f9dd
Galen Cook <galenscook@gmail.com>
2015-10-09 13:42:58 -0700

    init

---
+++ config.js
@@ -0,0 +1,16 @@
+// var dbConfig = {
+//   host: process.env.RDB_HOST || 'localhost',
+//   port: parseInt(process.env.RDB_PORT) || 28015,
+//   db  : process.env.RDB_DB || 'chat',
+//   tables: {
+//     'trucks': 'id',
+//     'cache': 'cid',
+//     'users': 'id'
+//   }
+// };
+
+// var connection = null;
+// r.connect( {host: 'localhost', port: 28015}, function(err, conn) {
+//     if (err) throw err;
+//     connection = conn;
+// })
---
+++ routes/index.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+var rdb = require('../lib/rethink');
+// var auth = require('../lib/auth');
+
+/* GET home page. */
+router.get('/', function(request, response, next) {
+  var newUser = {
+              name: "Steve",
+              email: "steve@yahoo.com",
+              password: "password"
+          };
+
+          rdb.save('users', newUser)
+          .then(function (result) {
+              console.log(newUser)
+              response.render('index', {title: (result.name)});
+              // console.log(response)
+          });
+
+
+  // res.render('index', { title: 'Express' });
+});
+
+router.get('/:id', function (request, response, next) {
+    rdb.find('users', request.params.id)
+    .then(function (user) {
+        if(!user) {
+            var notFoundError = new Error('User not found');
+            notFoundError.status = 404;
+            return next(notFoundError);
+        }
+        console.log(user)
+        response.json(user);
+    });
+});
+
+
+
+module.exports = router;
