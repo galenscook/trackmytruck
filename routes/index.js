ebadfc234eb98d23cd245b9793cd8dd576d61218
Galen Cook <galenscook@gmail.com>
2015-10-09 18:13:39 -0700

    Add user registration and profile routes

--- routes/index.js
+++ routes/index.js
@@ -4,36 +4,36 @@ var rdb = require('../lib/rethink');
 // var auth = require('../lib/auth');
 
 /* GET home page. */
-router.get('/', function(request, response, next) {
-  var newUser = {
-              name: "Steve",
-              email: "steve@yahoo.com",
-              password: "password"
-          };
+// router.get('/', function(request, response, next) {
+//   var newUser = {
+//               name: "Steve",
+//               email: "steve@yahoo.com",
+//               password: "password"
+//           };
 
-          rdb.save('users', newUser)
-          .then(function (result) {
-              console.log(newUser)
-              response.render('index', {title: (result.name)});
-              // console.log(response)
-          });
+//           rdb.save('users', newUser)
+//           .then(function (result) {
+//               console.log(newUser)
+//               response.render('index', {title: (newUser.name)});
+//               // console.log(response)
+//           });
 
 
-  // res.render('index', { title: 'Express' });
-});
+//   // res.render('index', { title: 'Express' });
+// });
 
-router.get('/:id', function (request, response, next) {
-    rdb.find('users', request.params.id)
-    .then(function (user) {
-        if(!user) {
-            var notFoundError = new Error('User not found');
-            notFoundError.status = 404;
-            return next(notFoundError);
-        }
-        console.log(user)
-        response.json(user);
-    });
-});
+// router.get('/:id', function (request, response, next) {
+//     rdb.find('users', request.params.id)
+//     .then(function (user) {
+//         if(!user) {
+//             var notFoundError = new Error('User not found');
+//             notFoundError.status = 404;
+//             return next(notFoundError);
+//         }
+//         console.log(user)
+//         response.json(user);
+//     });
+// });
 
 
 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,58 @@
 var express = require('express');
 var router = express.Router();
+var rdb = require('../lib/rethink');
+var auth = require('../lib/auth');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+
+// auth.authorize,
+
+// New User Form
+
+router.get('/new', function(request, response, next) {
+    response.render('users/new');
+});
+
+
+// Show User Profile
+
+router.get('/:id', function (request, response, next) {
+  rdb.find('users', request.params.id)
+  .then(function (user) {
+    if(!user) {
+      var notFoundError = new Error('User not found');
+      notFoundError.status = 404;
+      return next(notFoundError);
+    }
+    response.render('users/show', {user: user});
+  });
+});
+
+// Creates new user in database
+router.post('/', function (request, response) {
+  auth.hash_password(request.body.password)
+  .then(function (hash) {
+    var newUser = {
+      name: request.body.name,
+      email: request.body.email,
+      cell: request.body.cell,
+      password: hash,
+      updated_at: rdb.now()
+    };
+
+    rdb.save('users', newUser)
+    .then(function (result) {
+      rdb.findBy('users', 'email', newUser.email)
+      .then(function(users){
+        response.redirect('/users/'+users[0].id)
+      })
+
+    });
+  });
 });
 
 module.exports = router;
