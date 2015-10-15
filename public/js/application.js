c51e8688cd1397aff246f8b6691e85b9db274cd7
Galen Cook <galenscook@gmail.com>
2015-10-14 21:47:10 -0700

    Add tabs on registration pages

--- public/js/application.js
+++ public/js/application.js
@@ -2,22 +2,23 @@ $(document).ready(function() {
   freezeWindow();
   function setHeight() {
     windowHeight = $(window).innerHeight();
+    $('#content').css('height', (windowHeight));
     if(windowHeight <= 450){
 
       $('#truckmap').css('min-height', (windowHeight*0.5));
       $('#truckmap').css('max-height', (windowHeight*0.5));
 
     } else if(windowHeight <= 625){
-      $('#sidebar').css('min-height', (windowHeight))
+      $('#sidebar').css('min-height', (windowHeight));
       $('#truckmap').css('min-height', (windowHeight));
       $('#trucklist').css('height', (windowHeight*0.65));
     } else if(windowHeight <= 1120){
-      $('#sidebar').css('min-height', (windowHeight))
+      $('#sidebar').css('min-height', (windowHeight));
       $('#truckmap').css('min-height', (windowHeight));
       $('#trucklist').css('height', (windowHeight*0.7));
     } else {
 
-    $('#sidebar').css('min-height', (windowHeight))
+    $('#sidebar').css('min-height', (windowHeight));
     $('#truckmap').css('min-height', (windowHeight));
     $('#trucklist').css('height', (windowHeight*0.75));
     // $('#trucklist').css('max-height', (windowHeight*0.65));
@@ -28,6 +29,9 @@ $(document).ready(function() {
   $(window).resize(function() {
     setHeight();
   });
+
+  tabbedContent();
+
 });
 
 function freezeWindow(){
@@ -35,4 +39,21 @@ function freezeWindow(){
   if(route.test($(location).attr('href'))){
     $('body').css('position', 'fixed')
   }
+}
+
+function tabbedContent(){
+  var initialContent = $('li.active a').attr('href');
+  $(initialContent).css("display", "block")
+  
+  $('#tabbed-content li a').click(function(){
+    event.preventDefault();
+
+    $('#tabbed-content li').removeClass('active');
+    $(this).parent().addClass('active');
+
+    var currentContent = $('li.active a').attr('href');
+    $(".tab").css("display", "none");
+    $(currentContent).css("display", "block");
+
+  });
 }
